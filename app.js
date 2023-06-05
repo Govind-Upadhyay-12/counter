0eac09b57f261fc93a09b69618cd3ecfabc2ec03
Govind Upadhyay <119063599+Govind-Upadhyay-12@users.noreply.github.com>
2023-06-06 01:23:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let count=0;
+let value=document.querySelector('#value');
+let btns=document.querySelectorAll('.container');
+
+btns.forEach(function(items){
+btns.addEventListner('click',function(e){
+console.log(e.target);
+   });
+
+});
+
